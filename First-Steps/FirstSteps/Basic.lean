da8e9afed6f1e2586dc04b8d5fa43ac6a20ee024
Francesco Macrì <53121291+francescomacri@users.noreply.github.com>
2024-11-10 23:26:17 +0100

    Dabbling around with the first functions.

--- First-Steps/FirstSteps/Basic.lean
+++ First-Steps/FirstSteps/Basic.lean
@@ -1,2 +1,13 @@
 /- def hello := "world" -/
 def main : IO Unit := IO.println "Hello, world!"
+
+def add1 (n : Nat) : Nat := n + 1
+
+/- Here the function add1 is called and the integer 7 is added -/
+#eval add1 7
+
+/- The same program, but with 7.0 being a folat: is not possible, because the function is defined with natural numbers -/
+#eval add1 7
+
+/- This is an addition of floats, returning floats. -/
+#eval 1.2 + 2.8
