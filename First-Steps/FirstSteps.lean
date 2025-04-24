28bbadf95035cd25a4c556dfc0ee50683fed3c07
Francesco Macrì <53121291+francescomacri@users.noreply.github.com>
2025-04-24 14:37:29 +0200

    Update FirstSteps.lean

--- First-Steps/FirstSteps.lean
+++ First-Steps/FirstSteps.lean
@@ -8,6 +8,6 @@ import FirstSteps.Basic
 
 example {a b : ℚ} (h1 : a - b = 4) (h2 : a * b = 1) : (a + b) ^ 2 = 20 :=
 calc
-  (a + b) ^ 2 = (a - b) ^ 2 + 4*(a*b) := by ring
+  (a + b) ^ 2 = (a - b) ^ 2 + 4 * (a * b) := by ring
 _ = 4 ^ 2 + 4 * 1 := by rw [h1, h2]
 _ = 20 := by ring
