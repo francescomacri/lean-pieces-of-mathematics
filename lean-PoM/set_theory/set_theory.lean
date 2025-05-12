f0e2e4db1b8f2a5af8b677a105b58cd103ef9030
Francesco Macrì <53121291+francescomacri@users.noreply.github.com>
2025-05-12 08:02:44 +0200

    Update set_theory.lean

--- lean-PoM/set_theory/set_theory.lean
+++ lean-PoM/set_theory/set_theory.lean
@@ -1,1 +1,12 @@
 -- set_theory.lean
+
+def twice (f : Nat -> Nat) (a : Nat) :=
+f (f a)
+
+#eval println! twice (fun x => x + 2) 10
+
+theorem twiceAdd2 (a: Nat) : twice (fun x => x + 2) a = a + 4 :=
+-- True proof is by reflexivity. Lean "symbolically" reduces both sides of the equality until they are identical.
+rfl
+
+-- continuing on the page https://lean-lang.org/documentation/tour/ ...here: ...(* + 2)
